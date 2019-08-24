39b157dd0455f2eaa4aa9dadf3c06279be75103b
kaushik853 <kaushik853@gmail.com>
2019-08-25 01:30:48 +0200

    bot

---
+++ tweepy-bots/config.py
@@ -0,0 +1,24 @@
+import os
+import tweepy
+from pprint import pprint
+import logging
+
+logger = logging.getLogger()
+def create_api():
+# fetch the secrets from our virtual environment variables
+    CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+    CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+    ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+    ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+    # authenticate to the service we're accessing
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
